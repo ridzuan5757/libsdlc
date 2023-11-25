c194806e4f5da707717d0c915a6c802b8024c50e
ridzuan5757 <ridzuan5757@protonmail.com>
2023-11-25 17:07:39 +0800

    texture

--- texture/main.cpp
+++ texture/main.cpp
@@ -3,6 +3,7 @@
 #include "SDL_video.h"
 #include <SDL.h>
 #include <stdio.h>
+#include <string>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -31,6 +32,49 @@ bool init() {
     }
   }
   return success;
+}
+
+SDL_Surface *loadSurface(std::string fpath) {
+  SDL_Surface *optimizedSurface = NULL;
+  SDL_Surface *loadedSurface = SDL_LoadBMP(fpath.c_str());
+
+  if (loadedSurface == NULL) {
+    printf("Unalbe to load %s image. SDL error: %s.\n", fpath.c_str(),
+           SDL_GetError());
+  } else {
+    optimizedSurface = SDL_ConvertSurface(loadedSurface, surface->format, 0);
+
+    if (optimizedSurface == NULL) {
+      printf("Unable to optimize %s image. SDL_Error: %s.\n", fpath.c_str(),
+             SDL_GetError());
+    }
+
+    printf("Optimization completed.");
+    SDL_FreeSurface(loadedSurface);
+  }
+  return optimizedSurface;
+}
+
+bool loadMedia(std::string fpath) {
+  bool success = true;
+  splashSurface = loadSurface(fpath);
+
+  if (splashSurface == NULL) {
+    printf("Unable to load %s image. SDL error: %s.\n", fpath.c_str(),
+           SDL_GetError());
+    success = false;
+  }
+  return success;
+}
+
+void close() {
+  SDL_FreeSurface(splashSurface);
+  splashSurface = NULL;
+
+  SDL_DestroyWindow(window);
+  window = NULL;
+
+  SDL_Quit();
 }
 
 int main(int arc, char *args[]) { return 0; }
